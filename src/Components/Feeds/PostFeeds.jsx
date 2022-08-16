827ec183731ed2823a71498004074970eb249240
Ola Ishola <olaishola@hotmail.co.uk>
2022-08-16 17:29:20 +0100

    Removed component fetching

--- src/Components/Feeds/PostFeeds.jsx
+++ src/Components/Feeds/PostFeeds.jsx
@@ -1,34 +1,22 @@
+/* eslint-disable no-sequences */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import axios from 'axios';
-
-// const baseUrl = process.env.REACT_APP_BASE_URL;
 
 function PostFeeds(props) {
-  const [users, setUsers] = React.useState([]);
+  // const [users, setUsers] = React.useState([]);
   const data = props.posts || [];
 
-  const fetchUsers = async () => {
-    const response = await axios.get('http://localhost:3000/api/auth/users');
-    setUsers(response.data);
-  };
-
-  React.useEffect(() => {
-    // fetchUsers();
-
-  }, [users, data]);
-
-  // console.log(users);
   return (
     <div>
       {data.map((post) => (
-        <ul key={post.id}>
-          <li>
-            <p>{post.text}</p>
-            <img src={post.image_url} alt={post.content} />
-          </li>
-        </ul>
+        console.log(post),
+          <ul key={post.id}>
+            <li>
+              <p>{post.text}</p>
+              <img src={`http://127.0.0.1:3000/${post.image_url.url}`} alt={post.content} />
+            </li>
+          </ul>
       )).reverse()}
     </div>
   );
