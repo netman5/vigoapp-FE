8cbb6ef2ebbad4a028c21d5cc8c6cc919b86d226
Ola Ishola <olaishola@hotmail.co.uk>
2022-08-20 12:49:55 +0100

    Add user name to post and create style module for post feeds

--- src/Components/Feeds/PostFeeds.jsx
+++ src/Components/Feeds/PostFeeds.jsx
@@ -1,17 +1,21 @@
-/* eslint-disable no-sequences */
-/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Avatar from '../../images/avatar.webp';
+import styles from './PostFeeds.module.css';
 
 function PostFeeds(props) {
-  // const [users, setUsers] = React.useState([]);
-  const data = props.posts || [];
+  const { posts } = props;
+  const data = posts || [];
 
   return (
-    <div>
+    <div className={styles.feeds}>
       {data.map((post) => (
         <ul key={post.id}>
           <li>
+            <div className={styles.postDetails}>
+              <img src={Avatar} alt="post" className={styles.postImage} />
+              <h3>{post.name || 'Anonymous'}</h3>
+            </div>
             <p>{post.text}</p>
             <img src={post.image_url} alt={post.content} />
           </li>
