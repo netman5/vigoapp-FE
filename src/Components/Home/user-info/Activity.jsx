133dc9aa3f3fa3ec8ba585b0257c2e6a00d1f3e9
Ola Ishola <olaishola@hotmail.co.uk>
2022-08-31 15:45:44 +0100

    Populate activity section with dummy data with styling

--- src/Components/Home/user-info/Activity.jsx
+++ src/Components/Home/user-info/Activity.jsx
@@ -1,25 +1,112 @@
-import React from 'react';
+import React, { useId } from 'react';
+import { HiUserGroup } from 'react-icons/hi';
+import { BsCalendar4Event } from 'react-icons/bs';
+import { RiHashtag } from 'react-icons/ri';
 import styles from './UserInfo.module.css';
 
 function Activity() {
+  const id = useId();
+  const groups = [
+    {
+      id,
+      name: 'Sales Yard',
+    },
+    {
+      id,
+      name: 'Open Market',
+    },
+    {
+      id,
+      name: 'Book Warmer',
+    },
+    {
+      id,
+      name: 'The Thinkers',
+    },
+    {
+      id,
+      name: 'Neighborhood Watch',
+    },
+  ];
+
+  const events = [
+    {
+      id,
+      name: 'Cooking',
+    },
+    {
+      id,
+      name: 'JSWorld Conference',
+    },
+    {
+      id,
+      name: 'BedFord River Carnival',
+    },
+    {
+      id,
+      name: 'Summer Job Festival',
+    },
+    {
+      id,
+      name: 'ReactJS Conference',
+    },
+  ];
+
+  const hashtags = [
+    {
+      id,
+      name: 'Postive Thinking',
+    },
+    {
+      id,
+      name: 'WAGMI',
+    },
+    {
+      id,
+      name: 'Coding',
+    },
+    {
+      id,
+      name: 'bugs',
+    },
+  ];
+
   return (
     <div className={styles.container}>
-      <div className={styles.group}>
-        <ul>
-          <li>Sales</li>
-        </ul>
+      <div className={styles.groups}>
+        <h3>Groups</h3>
+        {groups.map((group) => (
+          <ul key={group.id}>
+            <li className={styles.lists}>
+              <HiUserGroup />
+              <span>{group.name}</span>
+            </li>
+          </ul>
+        ))}
       </div>
 
       <div className={styles.events}>
-        <ul>
-          <li>Sales</li>
-        </ul>
+        <h3>Events</h3>
+        {events.map((event) => (
+          <ul key={event.id}>
+            <li className={styles.lists}>
+              <BsCalendar4Event />
+              <span>{event.name}</span>
+            </li>
+          </ul>
+        ))}
       </div>
 
-      <div className={styles.hastags}>
-        <ul>
-          <li>Sales</li>
-        </ul>
+      <div className={styles.hashtags}>
+        <h3>HashTags</h3>
+        {hashtags.map((tag) => (
+          <ul key={tag.id}>
+            <li>
+              <RiHashtag />
+              <span>{tag.name}</span>
+            </li>
+          </ul>
+        ))}
       </div>
     </div>
   );
