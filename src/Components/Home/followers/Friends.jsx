38a0f630e68c9f5ca63a8b3f35385b5189ad2b7d
Ola Ishola <olaishola@hotmail.co.uk>
2022-09-05 17:01:10 +0100

    Render dummy followers with styling

--- src/Components/Home/followers/Friends.jsx
+++ src/Components/Home/followers/Friends.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
+import { id } from '../../../utils/utils';
+import styles from './Followers.module.css';
+import Avatar from '../../../images/avatar.webp';
 
 function Friends() {
+  const followers = [
+    {
+      id: id(),
+      name: 'Musa Jugunu',
+      imageUrl: '',
+
+    },
+
+    {
+      id: id(),
+      name: 'Yahaya Muniru',
+      imageUrl: '',
+
+    },
+
+    {
+      id: id(),
+      name: 'Jayden Adagba',
+      imageUrl: '',
+
+    },
+
+    {
+      id: id(),
+      name: 'Peter Aganba',
+      imageUrl: '',
+
+    },
+  ];
+
   return (
-    <div>Friends</div>
+    <div className={styles.followers}>
+      <ul className={styles.followerLists}>
+        {followers.map((follower) => (
+          <li key={follower.id} className={styles.follower}>
+            <img src={follower.imageUrl || Avatar} alt="" />
+            <span>{follower.name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
