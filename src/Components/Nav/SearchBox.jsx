6a88d579721e6657181549485c768d8c07d916b4
Ola Ishola <olaishola@hotmail.co.uk>
2022-09-07 16:57:06 +0100

    Create a searchbox component in nav folder

---
+++ src/Components/Nav/SearchBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBox = () => {
+  const searchInput = 'Hello';
+  return (
+    <div>
+      {searchInput}
+    </div>
+  );
+};
+
+export default SearchBox;
